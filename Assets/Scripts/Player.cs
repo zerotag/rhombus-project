8a86d156083e1bddc9cc90ed68f60e5b1bc82371
zeroTAG <zerotag@users.noreply.github.com>
2022-01-11 01:18:46 -0300

    Movement and Manual Collision Detection

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    private BoxCollider2D boxCollider;
+    private Vector3 moveDelta;
+    private RaycastHit2D hit;
+
+    void Start() {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    void FixedUpdate() {
+        // Get Input
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        // Reset MoveDelta
+        moveDelta = new Vector3(x, y, 0);
+
+        // Swap sprite direction
+        if (moveDelta.x > 0) {
+            transform.localScale = Vector3.one;
+        } else if (moveDelta.x < 0) {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        // Check Collision Y
+        hit = Physics2D.BoxCast(
+            transform.position,
+            boxCollider.size,
+            0,
+            new Vector2(0, moveDelta.y),
+            Mathf.Abs(moveDelta.y * Time.deltaTime),
+            LayerMask.GetMask("Actor", "Blocking")
+        );
+
+        if (hit.collider == null) {
+            // Make it move
+            transform.Translate(0, moveDelta.y * Time.deltaTime, 0);
+        }
+
+        // Check Collision X
+        hit = Physics2D.BoxCast(
+            transform.position,
+            boxCollider.size,
+            0,
+            new Vector2(moveDelta.x, 0),
+            Mathf.Abs(moveDelta.x * Time.deltaTime),
+            LayerMask.GetMask("Actor", "Blocking")
+        );
+
+        if (hit.collider == null) {
+            // Make it move
+            transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
+        }
+
+    }
+}
